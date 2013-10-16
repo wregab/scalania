27787667f7fdeae47bffb3deec9466bffdfe1ca3
unknown <bwrega@gmail.com>
2013-10-16 22:11:11 +0200

    P16 formatting

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P16.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P16.scala
@@ -3,10 +3,12 @@ package pl.japila.scalania.s99
 object S99_P16 {
   def drop[T](n: Int, ts: Seq[T]): Seq[T] =
     ts.foldLeft((0, List[T]())) {
-      case ((nr, filtered), t) => (nr + 1, 
-									{ if ((nr + 1) % n == 0) 
-										filtered 
-									  else 
-										filtered :+ t })
+      case ((nr, filtered), t) => (nr + 1,
+        {
+          if ((nr + 1) % n == 0)
+            filtered
+          else
+            filtered :+ t
+        })
     }._2
 }
