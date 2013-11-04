ef68635fad1512919dff6248b76056c62762421d
unknown <bwrega@gmail.com>
2013-11-04 23:24:02 +0100

    S99_P28 solution

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P28.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P28.scala
@@ -2,5 +2,13 @@ package pl.japila.scalania.s99
 
 object S99_P28 {
   def lsort[T](ts: Seq[Seq[T]]): Seq[Seq[T]] = ???
-  def lsortFreq[T](tss: Seq[Seq[T]]): Seq[Seq[T]] = ???
+  def lsortFreq[T](tss: Seq[Seq[T]]): Seq[Seq[T]] = 
+    tss.foldRight(Map[Int, Seq[Seq[T]]]())((e, res) =>
+      //      res.get(e.length) match {
+      //      case None => res + ((e.length, List(e)))
+      //      case Some(prev) => res + ((e.length, e +: prev))
+      //    }
+      res + ((e.length, e +: res.getOrElse(e.length, Seq[Seq[T]]())))
+    ).toList.sortWith(_._2.length <= _._2.length)
+      .flatMap(_._2)
 }
